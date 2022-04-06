878e82b4b648df1f8558c96e7b88ac3a82f2b3fc
AnnaKopyrina <annakopyrina@mail.ru>
2022-04-06 20:06:49 +0300

    Unit2

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("HELLO WORLD!");
+    Scanner scan = new Scanner(System.in);
+        System.out.println("Введите Ваше имя");
+    String user = scan.nextLine();
+        System.out.println("Здравствуйте"+ user);
+    }
+}
